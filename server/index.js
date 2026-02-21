90f044d31bcca054c3ef714e93013765b86d9b0f
aakashbwp@gmail.com <aakashpathakece063@gmail.com>
2026-02-21 23:22:50 +0530

    Forgot something

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://bwp-kotoba.vercel.app",
+    origin: ["https://bwp-kotoba.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   },
