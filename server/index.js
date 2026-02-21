51884f8e9b937e38b2d357ec4174f412517bf378
aakashbwp@gmail.com <aakashpathakece063@gmail.com>
2026-02-21 23:11:29 +0530

    Fix socket

--- server/index.js
+++ server/index.js
@@ -19,7 +19,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://bwp-kotoba.vercel.app",
+    methods: ["GET", "POST"],
   },
 });
 
