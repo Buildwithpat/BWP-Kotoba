f0e4ee55e94b6cd81baf21c9923fe08b355185cf
aakashbwp@gmail.com <aakashpathakece063@gmail.com>
2026-02-22 00:57:17 +0530

    added import socket

--- client/src/pages/Identity.jsx
+++ client/src/pages/Identity.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import socket from "../socket";
 
 // DARK avatars
 import avatar1Dark from "../assets/avatars/dark/avatar1.svg";
